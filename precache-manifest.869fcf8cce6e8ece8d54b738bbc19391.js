b548437821eadd4450ef56d97236282c2402bc6a
sanxyy <sanxyy@gmail.com>
2019-08-19 17:36:08 +0800

    Updates

---
+++ precache-manifest.869fcf8cce6e8ece8d54b738bbc19391.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766cd289cdc26e5eb4117b7196017acb",
+    "url": "/react-js-test/index.html"
+  },
+  {
+    "revision": "81c1914c65106cf8d831",
+    "url": "/react-js-test/static/css/main.1ac77c8a.chunk.css"
+  },
+  {
+    "revision": "a87a2dc4be936e6b7a93",
+    "url": "/react-js-test/static/js/2.6fe939e6.chunk.js"
+  },
+  {
+    "revision": "81c1914c65106cf8d831",
+    "url": "/react-js-test/static/js/main.8f3ac175.chunk.js"
+  },
+  {
+    "revision": "d6102cec6fef93b3fe3d",
+    "url": "/react-js-test/static/js/runtime~main.ebf195ab.js"
+  }
+]);
